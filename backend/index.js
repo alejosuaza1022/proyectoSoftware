0f6f753cc916461bdd66f934e50595577427270e
alejandro suaza <alejandrosuaza.1022@gmail.com>
2020-05-03 01:25:41 -0400

    carpeta backend eliminada

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-require('dotenv').config()
-
-const app = express();
-app.use(express.json());
-
-app.listen(process.env.PORT, () => {
-    console.log(process.env.PORT);
-});
